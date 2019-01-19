1a3cc997bb3fbec9251820a55faff6ee2fc42184
Yurii Sydorets <Sirnick@ukr.net>
2019-01-20 00:49:38 +0200

    Code refactoring

--- background.js
+++ background.js
@@ -1,148 +1,194 @@
-browser.storage.onChanged.addListener(function (changes, areaName) {
-	for (key in changes) {
-		var storageChange = changes[key];
-		if(key == "defaultFolder") {
-			localStorage.defaultFolder = storageChange.newValue;
-		}else {
-			localStorage.rules = storageChange.newValue;
-		}
-	}
+const DEFAULT_FOLDER = "defaultFolder";
+const TAB_URL = "/dowload-sorter.html";
+
+browser.storage.onChanged.addListener((changes) => {
+    for (key in changes) {
+        var storageChange = changes[key];
+        if (key == DEFAULT_FOLDER) {
+            localStorage.defaultFolder = storageChange.newValue;
+        } else {
+            localStorage.rules = storageChange.newValue;
+        }
+    }
 });
 
-browser.runtime.onInstalled.addListener(function(details){
-    if(details.reason == "install"){
+browser.runtime.onInstalled.addListener((details) => {
+    if (details.reason == "install") {
+        browser.storage.sync.get(DEFAULT_FOLDER, (items) => {
+            if (typeof items.defaultFolder == 'undefined') {
+                localStorage.defaultFolder = "default";
+                browser.storage.sync.set({
+                    DEFAULT_FOLDER: "default"
+                });
+            } else {
+                localStorage.defaultFolder = items.defaultFolder;
+            }
+        });
 
-		browser.storage.sync.get('defaultFolder', function (items) {
-			if(typeof items.defaultFolder == 'undefined') {
-				localStorage.defaultFolder = "default";
-				browser.storage.sync.set({'defaultFolder': "default" });
+        browser.storage.sync.get('rules', (items) => {
+            if (typeof items.rules == 'undefined') {
+                localStorage.rules = JSON.stringify([{
+                    "extension": "jpg,jpeg,gif,png",
+                    "foldername": "images"
+                }, {
+                    "extension": "zip,z7,tar,gz",
+                    "foldername": "compression"
+                }, {
+                    "extension": "exe",
+                    "foldername": "exe"
+                }, {
+                    "extension": "pdf,hwp,doc,docx",
+                    "foldername": "document"
+                }, {
+                    "extension": "z*",
+                    "foldername": "z_start_all_file"
+                }]);
+                browser.storage.sync.set({
+                    'rules': JSON.stringify([{
+                        "extension": "jpg,jpeg,gif,png",
+                        "foldername": "images"
+                    }, {
+                        "extension": "zip,z7,tar,gz",
+                        "foldername": "compression"
+                    }, {
+                        "extension": "exe",
+                        "foldername": "exe"
+                    }, {
+                        "extension": "pdf,hwp,doc,docx",
+                        "foldername": "document"
+                    }, {
+                        "extension": "z*",
+                        "foldername": "z_start_all_file"
+                    }])
+                });
+            } else {
+                localStorage.rules = items.rules;
+            }
+            browser.tabs.create({
+                url: TAB_URL
+            });
+        });
 
-			}else {
-				localStorage.defaultFolder = items.defaultFolder;
-			}
-		});
-		browser.storage.sync.get('rules', function (items) {
-			if(typeof items.rules == 'undefined') {
-				localStorage.rules = JSON.stringify([{"extension":"jpg,jpeg,gif,png","foldername":"images"},{"extension":"zip,z7,tar,gz","foldername":"compression"},{"extension":"exe","foldername":"exe"},{"extension":"pdf,hwp,doc,docx","foldername":"document"},{"extension":"z*","foldername":"z_start_all_file"}]);
-				browser.storage.sync.set({'rules': JSON.stringify([{"extension":"jpg,jpeg,gif,png","foldername":"images"},{"extension":"zip,z7,tar,gz","foldername":"compression"},{"extension":"exe","foldername":"exe"},{"extension":"pdf,hwp,doc,docx","foldername":"document"},{"extension":"z*","foldername":"z_start_all_file"}]) });
-			}else {
-				localStorage.rules = items.rules;
-			}
-			browser.tabs.create({url: "/dowload-sorter.html"});
-		});
+    } else if (details.reason == "update") {
+        browser.storage.sync.get(DEFAULT_FOLDER, (items) => {
+            if (typeof items.defaultFolder == 'undefined') {
+                browser.storage.sync.set({
+                    DEFAULT_FOLDER: localStorage.defaultFolder
+                });
+            } else {
+                localStorage.defaultFolder = items.defaultFolder;
+            }
+        });
 
-    }else if(details.reason == "update"){
-
-		browser.storage.sync.get('defaultFolder', function (items) {
-			if(typeof items.defaultFolder == 'undefined') {
-				browser.storage.sync.set({'defaultFolder': localStorage.defaultFolder});
-			}else {
-				localStorage.defaultFolder = items.defaultFolder;
-			}
-		});
-		browser.storage.sync.get('rules', function (items) {
-			if(typeof items.rules == 'undefined') {
-				browser.storage.sync.set({'rules': localStorage.rules});
-			}else {
-				localStorage.rules = items.rules;
-			}
-			browser.tabs.create({url: "/dowload-sorter.html"});
-		});
+        browser.storage.sync.get('rules', (items) => {
+            if (typeof items.rules == 'undefined') {
+                browser.storage.sync.set({
+                    'rules': localStorage.rules
+                });
+            } else {
+                localStorage.rules = items.rules;
+            }
+            browser.tabs.create({
+                url: TAB_URL
+            });
+        });
     }
 });
 
 function matches(extension, filename) {
-	if(extension == ""){
-		return false;
-	}
-	
-	index = -1;
-	index = filename.lastIndexOf('.');
-	
-	if(index != -1){
-		type = filename.substring(index+1, filename.len);
-	}
+    if (extension == "") {
+        return false;
+    }
 
-	extensionLower = extension.toLowerCase();
-	extensionLower = extensionLower.replace(/ /gi, '');
+    index = -1;
+    index = filename.lastIndexOf('.');
 
-	if(extensionLower.indexOf("*") != -1 || extensionLower.indexOf("|") != -1 || extensionLower.indexOf(",") != -1) {
+    if (index != -1) {
+        type = filename.substring(index + 1, filename.len);
+    }
 
-		extensionLower = extensionLower.replace(/,/gi, '|');
+    extensionLower = extension.toLowerCase();
+    extensionLower = extensionLower.replace(/ /gi, '');
 
-		extensionSplit = extensionLower.split('|');
+    if (extensionLower.indexOf("*") != -1 ||
+        extensionLower.indexOf("|") != -1 ||
+        extensionLower.indexOf(",") != -1) {
 
-		temp = "";
-		index = 0;
-		extensionSplit.forEach(function(element) {
-			if(index != 0)
-				temp += "|^" + element + "$";
-			else
-				temp += "^" + element + "$";
-			index++;
-		});
+        extensionLower = extensionLower.replace(/,/gi, '|');
+        extensionSplit = extensionLower.split('|');
 
-		extensionLower = temp;
+        temp = "";
+        index = 0;
+        extensionSplit.forEach((element) => {
+            if (index != 0)
+                temp += "|^" + element + "$";
+            else
+                temp += "^" + element + "$";
+            index++;
+        });
 
-		extensionLower = extensionLower.replace(/\*/gi, "[a-z0-9]*");
+        extensionLower = temp;
+        extensionLower = extensionLower.replace(/\*/gi, "[a-z0-9]*");
 
-		var pattern = new RegExp(extensionLower);
+        var pattern = new RegExp(extensionLower);
+        if (pattern.test(type.toLowerCase())) {
+            return true;
+        }
+    }
 
-		if(pattern.test(type.toLowerCase())) {
-			return true;
-		}
-	}
+    if (type.toLowerCase() == extensionLower) {
+        return true;
+    }
 
-	if(type.toLowerCase() == extensionLower) {
-		return true;
-	}
-	
-	return false;
+    return false;
 }
 
+const CONTEXT_ID = "save-file";
 browser.contextMenus.create({
-    id: "save-file",
+    id: CONTEXT_ID,
     title: "Save file to location",
     contexts: ["all"],
 });
-browser.contextMenus.onClicked.addListener((info, tab) => {
-if (info.menuItemId === "save-file") {
-	
-	var safeUrl = escapeHTML(info.linkUrl);
 
-	if (safeUrl === 'undefined') {
-		safeUrl = escapeHTML(info.srcUrl);
-	}
-	var localFilename = safeUrl.replace(/^.*[\\\/]/, '');
+browser.contextMenus.onClicked.addListener((info) => {
+    if (info.menuItemId == CONTEXT_ID) {
+        function suggestDownload(filename, foldername) {
+            browser.downloads.download({
+                'filename': foldername + "/" + filename,
+                'saveAs': true,
+                'url': safeUrl
+            })
+        }
 
-	function suggest(filename, foldername) {		
-			
-			item = browser.downloads.download({'filename':foldername + "/" + filename, 'saveAs' : true, 'url':safeUrl})
-		}	
+        var safeUrl = "";
+        if (info.linkUrl != 'undefined') {
+            safeUrl = escapeHTML(info.linkUrl);
+        } else {
+            safeUrl = escapeHTML(info.srcUrl);
+        }
 
-	var rules = localStorage.rules;
+        var rules = localStorage.rules;
+        try {
+            rules = JSON.parse(rules);
+        } catch (e) {
+            localStorage.rules = JSON.stringify([]);
+        }
 
-	try {
-		rules = JSON.parse(rules);
-	} catch (e) {
-		localStorage.rules = JSON.stringify([]);
-	}
+        var localFilename = safeUrl.replace(/^.*[\\\/]/, '');
+        var found = false;
+        for (var index = 0; index < rules.length; ++index) {
+            var rule = rules[index];
+            if (matches(rule.extension, localFilename)) {
+                suggestDownload(localFilename, rule.foldername);
+                found = true;
+                break;
+            }
+        }
 
-	var found = false;
-	for ( var index = 0; index < rules.length; ++index) {
-		var rule = rules[index];
-		if (matches(rule.extension,localFilename)) {
-			suggest(localFilename, rule.foldername);
-			found = true;
-			break;
-		}
-	}
-	
-	
-	if(!found){
-		suggest(localFilename, localStorage.defaultFolder);
-	}
-}
+        if (!found) {
+            suggestDownload(localFilename, localStorage.defaultFolder);
+        }
+    }
 });
 
 // https://gist.github.com/Rob--W/ec23b9d6db9e56b7e4563f1544e0d546
--- firefox-downloads-sort.js
+++ firefox-downloads-sort.js
@@ -1,89 +1,80 @@
 function Rule(data) {
-	var rules = $('#rules > tbody');
-	this.node = $('#rule-template').clone();
-	this.node.removeAttr('id');
-	this.node.attr('class', 'rule');
-	Rule.next_id++;
-	this.node.find('.number').html(Rule.next_id);
-	rules.append(this.node);
+    var rules = $('#rules > tbody');
+    this.node = $('#rule-template').clone();
+    this.node.removeAttr('id');
+    this.node.attr('class', 'rule');
+    Rule.next_id++;
+    this.node.find('.number').html(Rule.next_id);
+    rules.append(this.node);
 
-	if (data) {
-		this.node.find('.number').text(Rule.next_id);
-		this.node.find('.extension').val(data.extension);
-		this.node.find('.foldername').val(data.foldername);
-	}
-	
-	this.node.find('.extension').on('keyup', function(){
-		storeRules();
-	});
-	
-	this.node.find('.foldername').on('keyup', function(){
-		storeRules();
-	});
+    if (data) {
+        this.node.find('.number').text(Rule.next_id);
+        this.node.find('.extension').val(data.extension);
+        this.node.find('.foldername').val(data.foldername);
+    }
 
-	this.node.find('.remove').on('click', function(){
-		_this = $(this).parent().parent()
-		
-		_this.nextAll().find('.number').each(function(index, element) {
-			number = $(element).text();
-			number--;
-			$(element).text(number);
-		});
-		_this.remove();
-		Rule.next_id--;
-		storeRules();
-	});
-	
-	storeRules();
+    this.node.find('.extension').on('keyup', () => storeRules());
+
+    this.node.find('.foldername').on('keyup', () => storeRules());
+
+    this.node.find('.remove').on('click', () => {
+        _this = $(this).parent().parent()
+
+        _this.nextAll().find('.number').each((index, element) => {
+            number = $(element).text();
+            number--;
+            $(element).text(number);
+        });
+        _this.remove();
+        Rule.next_id--;
+        storeRules();
+    });
+
+    storeRules();
 }
 
 Rule.next_id = 0;
 
 function loadRules() {
-
-	var rules = localStorage.rules;
-	try {
-		JSON.parse(rules).forEach(function(rule) {
-			new Rule(rule);
-		});
-	} catch (e) {
-		//localStorage.rules = JSON.stringify([]);
-	}
+    var rules = localStorage.rules;
+    try {
+        JSON.parse(rules).forEach((rule) => {
+            new Rule(rule);
+        });
+    } catch (e) {
+        //localStorage.rules = JSON.stringify([]);
+    }
 }
 
 function storeRules() {
-	var array = [];
-	
-	$('.rule').each(function(index, element) {
-		array.push({
-			extension : $(element).find('.extension').val(),
-			foldername : $(element).find('.foldername').val()
-		});
-	});
+    var rules = [];
+    $('.rule').each((index, element) => {
+        rules.push({
+            extension: $(element).find('.extension').val(),
+            foldername: $(element).find('.foldername').val()
+        });
+    });
 
-	localStorage.rules = JSON.stringify(array);
-
-	browser.storage.sync.set({'rules': JSON.stringify(array)});
-
+    localStorage.rules = JSON.stringify(rules);
+    browser.storage.sync.set({
+        'rules': JSON.stringify(rules)
+    });
 }
 
-function init(){
-   	$('#defaultFolder').val(localStorage.defaultFolder);
-
-	$('#defaultFolder').on('keyup', function(){
-
+function init() {
+    $('#defaultFolder').val(localStorage.defaultFolder);
+    $('#defaultFolder').on('keyup', () => {
         localStorage.defaultFolder = $('#defaultFolder').val();
-        
-		browser.storage.sync.set({'defaultFolder': $('#defaultFolder').val() });
-	});
-	
-	$('#new').on('click', function() {
-		new Rule();
-	});
-	
-	loadRules();
+        browser.storage.sync.set({
+            'defaultFolder': $('#defaultFolder').val()
+        });
+    });
+
+    $('#new').on('click', function() {
+        new Rule();
+    });
+
+    loadRules();
 }
 
-window.onload = function() {
-	init();
-};
+window.onload = () => init();
